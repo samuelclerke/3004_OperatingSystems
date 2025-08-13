558bee4c4f6feebba2beee850d06032ccc19cb37
samuelclerke <s.j.clerke@hotmail.com>
2025-08-13 18:35:25 +0930

    Changed all printf() to write().

--- even.c
+++ even.c
@@ -3,14 +3,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/*
+  Signal Handler
+   - Handles appropriate signals such as SIGHUP (1), and SIGINT (2), 
+     with correct responses by not killing the program and printing a response.
+   - Uses signal safe printing to terminal by utilising the write() function.
+*/
 void sigHandler(int sig) 
 {
+  const char *response = "";
+
   switch(sig) {
-    case 1:
-      write("Ouch!\n");
+    case 1: // SIGHUP
+      response = "Ouch!\n";
+      write(1, response, 6); // file_descriptor, response_msg, n_bytes
       break;
-    case 2:
-      write("Yeah!\n");
+    case 2: // SIGINT
+      response = "Yeah!\n";
+      write(1, response, 6); // file_descriptor, response_msg, n_bytes
       break;
   }
 
@@ -20,14 +30,18 @@ void sigHandler(int sig)
 int main(int argc, char** argv) 
 {
   int evenNum = 0;
+  char evenNumStr[12];
   int num = atoi(argv[1]);
 
   signal(SIGINT, sigHandler);
   signal(SIGHUP, sigHandler);
 
+  // Prints n even numbers.
   for (int i = 0; i < num; i++) 
   {
-    printf("%d\n", evenNum);
+    // Cast int evenNum to char[] evenNumStr for valid printing through write.
+    int len = snprintf(evenNumStr, sizeof(evenNumStr), "%d\n", evenNum);
+    write(1, evenNumStr, len); // file_descriptor, response_msg, n_bytes
     evenNum += 2;
     sleep(5);
   }
