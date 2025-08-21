df4b999a5e584bf88af0588a5d2b684f30649bb3
samuelclerke <s.j.clerke@hotmail.com>
2025-08-21 13:27:14 +0930

    #

--- minishell.c
+++ minishell.c
@@ -106,7 +106,7 @@ int jobs_running()
 int main(int argk, char *argv[], char *envp[])
 {
   int             frkRtnVal;	    /* value returned by fork sys call */
-  //int             wpid;		        /* value returned by wait */
+  int             wpid;		        /* value returned by wait */
   char           *v[NV];	        /* array of pointers to command line tokens */
   char           *sep = " \t\n";  /* command line token separators    */
   int             i;		          /* parse index */
@@ -195,7 +195,8 @@ int main(int argk, char *argv[], char *envp[])
         }
         else 
         {
-         wait(0);
+         wpid = wait(0);
+         wpid += 1;
         }
     	  break;
       }
